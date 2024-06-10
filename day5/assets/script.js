9f28a40a0af2ad7c6bea3ac102aa523864ba2df2
nanahiryu <ryu2751x7@gmail.com>
2024-06-10 16:09:52 +0900

    day5: 最低限の実装

---
+++ day5/assets/script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const categoryObj = {
+  comp: "企業情報",
+  prod: "製品",
+  ir: "IR 情報",
+};
+
+const assignCategoryClass = (tag, categoryName) => {
+  Object.keys(categoryObj).forEach((key) => {
+    if (categoryName === categoryObj[key]) {
+      tag.classList.add(`cat-${key}`);
+      return;
+    }
+  });
+};
+
+const setNewsList = (data) => {
+  const tableTag = document.getElementById("news-table");
+  data.forEach((datum) => {
+    const trTag = document.createElement("tr");
+    const dateTdTag = document.createElement("td");
+    const categoryTdTag = document.createElement("td");
+    const titleTdTag = document.createElement("td");
+    const titleATag = document.createElement("a");
+
+    dateTdTag.classList.add("date-cell");
+    categoryTdTag.classList.add("category-cell");
+    titleTdTag.classList.add("title-cell");
+
+    assignCategoryClass(categoryTdTag, datum.category.value);
+
+    dateTdTag.textContent = datum.day.value;
+    categoryTdTag.textContent = datum.category.value;
+    titleATag.textContent = datum.content.value;
+    titleATag.href = datum.url.value;
+    titleATag.target = datum.target.value;
+
+    titleTdTag.appendChild(titleATag);
+
+    trTag.appendChild(dateTdTag);
+    trTag.appendChild(categoryTdTag);
+    trTag.appendChild(titleTdTag);
+
+    tableTag.appendChild(trTag);
+  });
+};
+
+const fetchEventList = () => {
+  axios
+    .get(
+      "https://oykh3vmu623yt5rufrstzlyxyi0kitod.lambda-url.ap-northeast-1.on.aws/",
+      {
+        params: {
+          id: "dojo",
+        },
+      }
+    )
+    .then((resp) => {
+      setNewsList(resp.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+fetchEventList();
